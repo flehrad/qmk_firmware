25ac93171f8b769ec3d53f8007e5dd40b5257a15
noroadsleft <xxiinophobia@yahoo.com>
2018-12-31 20:41:02 -0800

    Snagpad: make it compile
    This commit updates the LAYOUT_numpad macro and default keymap so the
    keymap will compile.

--- keyboards/snagpad/keymaps/default/keymap.c
+++ keyboards/snagpad/keymaps/default/keymap.c
@@ -12,16 +12,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LAYOUT_numpad(
 		KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, 
 		KC_P7, KC_P8, KC_P9, KC_PPLS, 
-		KC_P4, KC_P5, KC_P6, KC_NO, 
+		KC_P4, KC_P5, KC_P6,  
 		KC_P1, KC_P2, KC_P3, KC_PENT, 
-		KC_P0, KC_NO, KC_PDOT, KC_NO),
+		KC_P0,        KC_PDOT       ),
 };
-
-	switch (id) {
-
-	}
-	return MACRO_NONE;
-}
 
 void matrix_init_user(void) {
 }
